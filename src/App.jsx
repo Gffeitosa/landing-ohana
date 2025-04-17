12f485a9d95857680f2a43a1b004382b955f8f03
Gabriel Feitosa <gabrielffeitosa@hotmail.com>
2025-04-17 13:06:23 -0300

    Limpa estrutura e mantém somente projeto final

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="banner">
+        <h1>Agende seu exame de vista gratuito</h1>
+        <p>E descubra as melhores ofertas em óculos na <strong>Ótica Ohana</strong></p>
+        <a
+          href="https://wa.me/5581998355571"
+          className="btn-whatsapp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Agende agora pelo WhatsApp
+        </a>
+      </section>
+    </div>
+  );
+}
+
+export default App;
